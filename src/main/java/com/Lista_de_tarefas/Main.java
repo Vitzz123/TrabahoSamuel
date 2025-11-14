dc5f86c5c923e2276726832a6e4da38ed35dafb4
Vitor Augusto Cunha de Sousa <shameofvitor@gmail.com>
2025-11-14 12:49:57 -0300

    Update Main.java

--- src/main/java/com/Lista_de_tarefas/Main.java
+++ src/main/java/com/Lista_de_tarefas/Main.java
@@ -1,25 +1,21 @@
-// O pacote raiz do seu projeto
 package com.Lista_de_tarefas;
 
-// Importa as classes dos outros pacotes
 import com.Lista_de_tarefas.controller.Lista_de_tarefas_controller;
 import com.Lista_de_tarefas.repository.Lista_de_tarefas_repository;
 
 public class Main {
     
     public static void main(String[] args) {
-        // 1. "Inicialização do Sistema"
-        // Criamos as instâncias necessárias (usando os nomes das suas classes)
+
         Lista_de_tarefas_repository meuRepositorio = new Lista_de_tarefas_repository();
         Lista_de_tarefas_controller meuController = new Lista_de_tarefas_controller(meuRepositorio);
 
-        // 2. "Simulação de Interação do Usuário"
+
         
         System.out.println("--- Tentativa 1: Criar tarefa válida ---");
-        // Cria uma tarefa com descrição
         meuController.criarNovaTarefa("Estudar GRASP para o trabalho");
 
         System.out.println("\n--- Tentativa 2: Criar tarefa inválida ---");
-        meuController.criarNovaTarefa("    "); // Tenta criar tarefa vazia
+        meuController.criarNovaTarefa("    ");
     }
 }
